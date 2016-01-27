1ada38cb4129b9441ac822014c0bb97ce766daf5
RolanDroid <rolandosimoncelli8@gmail.com>
2016-01-27 14:56:42 +0100

    CyanPop-ify
    Signed-off-by: RolanDroid <rolandosimoncelli8@gmail.com>

--- cyanpop.mk
+++ cyanpop.mk
@@ -1,8 +1,8 @@
 $(call inherit-product, device/motorola/otus/full_otus.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common CYANPOP stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_otus
+PRODUCT_NAME := cyanpop_otus
 PRODUCT_RELEASE_NAME := MOTO E 3G
